b8abe0220de35196ccf7a37c9fb141d23e30c413
AmberFazal <Amberfazal1@gmail.com>
2018-05-23 05:30:13 -0700

    Order controller and Model

---
+++ project/project/Controllers/OrdersController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using project.Models;
+
+namespace project.Controllers
+{
+    public class OrdersController : Controller
+    {
+        // GET: Orders
+        Mydb db = new Mydb();
+        public ActionResult Index()
+        {
+
+            if (Session["id"] != null)
+            {
+                var data = db.orders.ToList();
+                ViewBag.ProductsList = db.products.ToList();
+                return View(data);
+            }
+            return RedirectToAction("Login", "Customer");
+        }
+        public ActionResult placeOrder()
+        {
+
+            if (Session["id"] == null)
+            {
+                return Redirect("~/Customer/Login");
+            }
+            var cart = db.cart.ToArray();
+            Orders ord = new Orders();
+            for (int i = 0; i < cart.Length; i++)
+            {
+                ord.uis = (int)Session["id"];
+                ord.pid = cart[i].pid;
+                ord.status = "Pending";
+                db.orders.Add(ord);
+                db.SaveChanges();
+                db.cart.Remove(cart[i]);
+                db.SaveChanges();
+            }
+            return Redirect("~/Products/Index");
+
+        }
+
+        public ActionResult RemoveOrder(int id)
+        {
+            if (Session["id"] == null)
+            {
+                return Redirect("~/Customer/Login");
+            }
+            var del = db.orders.Find(id);
+            db.orders.Remove(del);
+            db.SaveChanges();
+            Session["Cart"] = null;
+            return RedirectToAction("Index");
+        }
+
+
+    }
+
+}
--- project/project/Models/Mydb.cs
+++ project/project/Models/Mydb.cs
@@ -15,6 +15,7 @@ namespace project.Models
         public DbSet<customer> customer { get; set; }
         public DbSet<Products> products { get; set; }
         public DbSet<cart> cart { get; set; }
+        public DbSet<Orders> orders { get; set; }
         public DbSet<Admin> Admin { get; set; }
 
     }
